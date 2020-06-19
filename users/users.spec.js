75feceb75384faadf599cd04ee9bb318bdacede9
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-19 11:37:01 -0500

    added 2 tests for get users endpoint

--- users/users.spec.js
+++ users/users.spec.js
@@ -1,1 +1,39 @@
 //get user list test
+const supertest = require("supertest");
+
+const server = require("../api/server");
+
+
+
+describe("users-router.js", () => {
+
+    // test 1- returns 401 because route is restriced without credentials
+
+    describe("GET /", () => {
+        it("should return 401 because no credentials yet", () => {
+            return supertest(server)
+                .get("/api/users")
+                .then(res => {
+                    // jest assertion
+                    expect(res.status).toBe(401);
+                });
+        });
+
+
+    })
+
+    //test 2 - returning JSON
+
+    it("should return JSON", () => {
+        return supertest(server)
+            .get("/api/users")
+            .then(res => {
+                // jest assertion
+                expect(res.type).toMatch(/json/i);
+            });
+    });
+});
+
+
+
+
